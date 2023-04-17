f398a2ef0c34297ad828f8e9475377a95b035396
zaki_linux <mohammedkhanzaki@gmail.com>
2023-04-18 03:35:27 +0530

    18/4/23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyBindingComponent } from './components/property-binding/property-binding.component';
+import { ItemListComponent } from './components/property-binding/item-list/item-list.component';
+import { ItemDetailComponent } from './components/property-binding/item-detail/item-detail.component';
+import { AttributeBindingComponent } from './components/attribute-binding/attribute-binding.component';
+import { CompWithHostBindingComponent } from './components/attribute-binding/comp-with-host-binding.component';
+import { MyInputWithAttributeDecoratorComponent } from './components/attribute-binding/my-input-with-attribute-decorator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyBindingComponent,
+    ItemListComponent,
+    ItemDetailComponent,
+    AttributeBindingComponent,
+    CompWithHostBindingComponent,
+    MyInputWithAttributeDecoratorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/property-binding/property-binding.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-binding',
+  templateUrl: './property-binding.component.html',
+  styleUrls: ['./property-binding.component.css']
+})
+export class PropertyBindingComponent {
+title ='PB';
+itemImageUrl = '../assets/phone.svg';
+  isUnchanged = true;
+  classes = 'special';
+  parentItem = 'lamp';
+
+  currentItems = [{
+    id: 21,
+    name: 'phone'
+  }];
+
+  interpolationTitle = 'Interpolation';
+  propertyTitle = 'Property binding';
+
+  evilTitle = 'Template <script>alert("evil never sleeps")</script> Syntax';
+}
